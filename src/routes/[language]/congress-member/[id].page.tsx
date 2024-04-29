b4ad5a8a30102cb520d7dbde08dbdb28937e72a5
Serhii Mytrokhin <smitrokhin@brightgrove.com>
2024-04-28 21:44:20 -0500

    add congress member detail layout

--- src/routes/[language]/congress-member/[id].page.tsx
+++ src/routes/[language]/congress-member/[id].page.tsx
@@ -9,8 +9,6 @@ import Container from '../../../components/Container'
 import cn from 'clsx'
 
 export const congressMemberDetailsUrl = (memberID: string, language: string) => `/${language}/congress-member/${memberID}`;
-	//=> `/${language}/congress-member`;
-	//=> `/${language}/congress-member/:${memberID}`;
 
 export default function CongressMember({ params }) {
 	const language = useLanguage()
@@ -21,12 +19,66 @@ export default function CongressMember({ params }) {
 	const apiCall = useApiCached<CongressMemberResponse>(`/members/${params.id}`, 3);
 	const member = apiCall?.data?.member;
 
-	//const tableData = apiCall.data ? buildTableData(apiCall.data, text) : null;
-
 	return (<Container className={style.container}>
 		<DataLoader isLoading={apiCall.isLoading}>
 			{member && <>
-				MEMBER: {member.first_name} {member.last_name}
+				<div className={style['header']} >
+					<h2>{member.title}</h2>
+					<h1>{member.first_name} {member.last_name}</h1>
+
+					{member.party == 'D' && <p>Democratic Party</p>}
+					{member.party == 'R' && <p>Republican Party</p>}
+				</div>
+
+				<div className={style['member-content']}>
+
+					<section className={style['content']}>
+						<h2>Vote statistics</h2>
+
+						<p>
+							{member.total_votes && <>
+								<strong>Total votes</strong>
+								<span>{member.total_votes}</span>					
+							</>}
+							{member.missed_votes && <>
+								<strong>Missed votes</strong>
+								<span>{member.missed_votes_pct}%</span>
+							</>}
+							{member.votes_with_party_pct && <>
+								<strong>Votes with party</strong>
+								<span>{member.votes_with_party_pct}%</span>
+							</>}
+							{member.votes_against_party_pct && <>
+								<strong>Votes against party</strong>
+								<span>{member.votes_against_party_pct}%</span>
+							</>}
+						</p>
+					</section>
+
+					<section className={style['contact']}>
+						<h2>Contact Information</h2>
+						<p>
+							{member.url && <>
+								<strong>Profile on</strong>
+								<span><a target="_blank" href={member.url}>senate.gov</a></span>
+							</>}
+							{member.phone && <>
+								<strong>Phone</strong>
+								<a href={`tel:${member.phone}`}>{member.phone}</a>
+							</>}
+							{member.office && <>
+								<strong>Office</strong>
+								<span>{member.office}</span>
+							</>}
+						</p>
+
+						<div className={style['social-media']}>
+							{member.youtube_account && <a href={`https://youtube.com/@${member.youtube_account}`} target="_blank" className="icon"><i className="fab fa-youtube"></i></a>}
+							{member.twitter_account && <a href={`https://twitter.com/${member.twitter_account}`} target="_blank" className="icon"><i className="fab fa-twitter"></i></a>}
+							{member.facebook_account && <a href={`https://facebook.com/${member.facebook_account}`} target="_blank" className="icon"><i className="fab fa-facebook-f"></i></a>}
+						</div>
+					</section>
+				</div>
 			</>}
 		</DataLoader>
 	</Container>);
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -64,7 +64,7 @@ const MainLayout: Layout = ({ children, url }) => {
 			<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap" />
 			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
 			<script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TSJ66R2');`}</script>
-
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"/>
 			<link rel="icon" type="image/x-icon" href={favicon} />
 		</Head>
 		<Header {...header} />
